9bf38f83fd0715729e1276d1d78a81d9feaad6b0
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-01-26 13:51:29 +0800

    feat: darkmode for certification section

--- src/app/[locale]/about/sections/certification-section.tsx
+++ src/app/[locale]/about/sections/certification-section.tsx
@@ -1,5 +1,8 @@
 import { AboutMeContent } from "@/components";
 import { CERTIFICATION_EXPERIENCE, style } from "@/config";
+import { translate } from "@/lib";
+import { useTranslations } from "next-intl";
+import { use } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function CertificationSection() {
@@ -11,8 +14,8 @@ export default function CertificationSection() {
         style.sectionContainerPaddingX
       )}
     >
-      <h3 className="xl:text-h2 lg:text-h3 sm:text-h4 text-h5 font-semibold text-scorpion-600 text-center">
-        Certificates
+      <h3 className="xl:text-h2 lg:text-h3 sm:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
+        {translate(useTranslations(), "about.certificate.title")}
       </h3>
       <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
         {CERTIFICATION_EXPERIENCE.map((work, index) => (
