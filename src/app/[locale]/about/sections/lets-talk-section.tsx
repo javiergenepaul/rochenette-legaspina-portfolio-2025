cdc32649a31590e4fad9ff365b4758552d85b92c
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-01-26 13:59:13 +0800

    feat: darkmode for lets talk section

--- src/app/[locale]/about/sections/lets-talk-section.tsx
+++ src/app/[locale]/about/sections/lets-talk-section.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { style } from "@/config";
+import { translate } from "@/lib";
+import { useTranslations } from "next-intl";
 import { twMerge } from "tailwind-merge";
 
 export default function LetsTalkSection() {
@@ -7,17 +9,17 @@ export default function LetsTalkSection() {
     <section
       id="lets-talk-section"
       className={twMerge(
-        "bg-amethyst-100 xl:py-52 lg:py-40 py-20 flex justify-center items-center",
+        "bg-amethyst-100 dark:bg-woodsmoke-900 xl:py-52 lg:py-40 py-20 flex justify-center items-center",
         style.sectionContainerPaddingX
       )}
     >
       <div className="flex flex-col gap-16 items-center w-full ">
         <div className="flex flex-col">
-          <h2 className="xl:text-h1 lg:text-h2 sm:text-h3 text-h4 font-bold text-scorpion-400 text-center">
-            Have a concept in mind!
+          <h2 className="xl:text-h1 lg:text-h2 sm:text-h3 text-h4 font-bold text-scorpion-400 dark:text-woodsmoke-50 text-center">
+            {translate(useTranslations(), "about.letsTalk.title.1")}
           </h2>
-          <h2 className="xl:text-h1 lg:text-h2 sm:text-h3 text-h4 font-bold text-amethyst-400 text-center">
-            Connect with me.
+          <h2 className="xl:text-h1 lg:text-h2 sm:text-h3 text-h4 font-bold text-amethyst-400 dark:text-amethyst-400 text-center">
+            {translate(useTranslations(), "about.letsTalk.title.2")}
           </h2>
         </div>
         <div className="flex md:gap-6 gap-5 md:flex-row flex-col items-center sm:w-6/12 xs:w-8/12 w-full">
@@ -26,15 +28,18 @@ export default function LetsTalkSection() {
             variant={"default"}
           >
             <span className="xl:text-large xs:text-normal text-button">
-              Let's Talk
+              {translate(useTranslations(), "about.letsTalk.button.letsTalk")}
             </span>
           </Button>
           <Button
-            className="xl:px-14 px-5 md:px-14 xl:py-5 xs:py-3.5 w-full text-normal text-scorpion-700 border-scorpion-700 bg-transparent"
+            className="xl:px-14 px-5 md:px-14 xl:py-5 xs:py-3.5 w-full text-normal text-scorpion-700 border-scorpion-700 dark:border-amethyst-500 bg-transparent"
             variant={"outline"}
           >
-            <span className="xl:text-large xs:text-normal text-button">
-              Download my CV
+            <span className="xl:text-large xs:text-normal text-button dark:text-woodsmoke-50">
+              {translate(
+                useTranslations(),
+                "about.letsTalk.button.downloadMyCV"
+              )}
             </span>
           </Button>
         </div>
