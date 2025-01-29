ca5a101f6d8674e52d07bf0b6b2c24fe6956b0f8
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-01-29 21:39:11 +0800

    refactor: responsive prefix fix

--- src/app/[locale]/about/sections/skills-and-tools-section.tsx
+++ src/app/[locale]/about/sections/skills-and-tools-section.tsx
@@ -9,18 +9,18 @@ export default function SkillsAndToolsSection() {
     <section
       id="skills-and-tools-section"
       className={twMerge(
-        "flex flex-col lg:gap-10 gap-8",
+        "flex flex-col LG:gap-10 gap-8",
         style.sectionContainerPaddingX
       )}
     >
-      <h3 className="xl:text-h2 lg:text-h3 md:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
+      <h3 className="XL:text-h2 LG:text-h3 MD:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
         {translate(useTranslations(), "about.skillAndTools.title")}
       </h3>
       <div className="flex gap-6 mx-auto">
         {SKILLS_AND_TOOLS.map((skill, index) => (
           <div key={index} className="flex items-center justify-center">
             <Image
-              className="xl:h-28 xl:w-28 md:h-20 md:w-20 sm:h-16 sm:w-16 h-14 w-14 object-contain"
+              className="XL:h-28 XL:w-28 MD:h-20 MD:w-20 SM:h-16 SM:w-16 h-14 w-14 object-contain"
               src={skill.image}
               alt={skill.alt}
             />
