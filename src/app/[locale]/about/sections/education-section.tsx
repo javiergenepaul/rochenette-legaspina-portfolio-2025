14faf07fe8f4b24aedfffe9f8ad7cd8b76667601
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-01-29 21:39:11 +0800

    refactor: responsive prefix fix

--- src/app/[locale]/about/sections/education-section.tsx
+++ src/app/[locale]/about/sections/education-section.tsx
@@ -9,14 +9,14 @@ export default function EducationSection() {
     <section
       id="education-section"
       className={twMerge(
-        "flex flex-col lg:gap-10 gap-8",
+        "flex flex-col LG:gap-10 gap-8",
         style.sectionContainerPaddingX
       )}
     >
-      <h3 className="xl:text-h2 lg:text-h3 sm:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
+      <h3 className="XL:text-h2 LG:text-h3 SM:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
         {translate(useTranslations(), "about.education.title")}
       </h3>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+      <div className="grid MD:grid-cols-2 grid-cols-1 gap-6">
         {EDUCATION_EXPERIENCE.map((work, index) => (
           <AboutMeContent key={index} {...work} />
         ))}
--- src/app/[locale]/about/sections/work-section.tsx
+++ src/app/[locale]/about/sections/work-section.tsx
@@ -9,14 +9,14 @@ export default function WorkSection() {
     <section
       id="work-experience-section"
       className={twMerge(
-        "flex flex-col lg:gap-10 gap-8",
+        "flex flex-col LG:gap-10 gap-8",
         style.sectionContainerPaddingX
       )}
     >
-      <h3 className="xl:text-h2 lg:text-h3 sm:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
+      <h3 className="XL:text-h2 LG:text-h3 SM:text-h4 text-h5 font-semibold text-scorpion-600 dark:text-woodsmoke-50 text-center">
         {translate(useTranslations(), "about.work.title")}
       </h3>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+      <div className="grid MD:grid-cols-2 grid-cols-1 gap-6">
         {WORK_EXPERIENCE.map((work, index) => (
           <AboutMeContent key={index} {...work} />
         ))}
