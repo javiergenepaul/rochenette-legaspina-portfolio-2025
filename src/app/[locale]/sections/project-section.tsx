ea5c2a7c6f7c7e50b202731ddab856c1fbd540aa
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-02-02 13:46:00 +0800

    adding translate to project section in home page

--- src/app/[locale]/sections/project-section.tsx
+++ src/app/[locale]/sections/project-section.tsx
@@ -1,28 +1,38 @@
 import { style } from "@/config";
 import { twMerge } from "tailwind-merge";
 import ProjectContainer from "./components/project-container";
+import { translate } from "@/lib";
+import { useTranslations } from "next-intl";
 
 export default function ProjectSection() {
   const PROJECTS = [
     {
-      title: "CountryScape",
-      description:
-        "Countryscape offers a selection of hotels with stunning views that perfectly match your mood and emotions, providing a tailored vacation experience. Whether you're seeking relaxation, adventure, or a peaceful retreat, each hotel is designed to enhance your stay. With easy booking options, planning your ideal getaway has never been more convenient.",
+      title: translate(useTranslations(), "home.project.projects.1.title"),
+      description: translate(
+        useTranslations(),
+        "home.project.projects.1.description"
+      ),
     },
     {
-      title: "Food Delivery App",
-      description:
-        "Food Delivery and Transportation App that offers efficient services for food delivery, as well as transportation for people and goods. It allows users to manage their time better by eliminating the need to leave their home, office, or school for tasks like shopping or collecting items. Additionally the app offers secure and well monitored deliveries, ensures safe transportation and exclusive travel to destinations.",
+      title: translate(useTranslations(), "home.project.projects.2.title"),
+      description: translate(
+        useTranslations(),
+        "home.project.projects.2.description"
+      ),
     },
     {
-      title: "Web App",
-      description:
-        "The networking website offers a curated selection of exclusive products, available only through the platform. Focused on health and wellness, the products are carefully chosen to support a balanced lifestyle and promote well-being. Users can access unique items in medicine, supplements, and wellness tools, ensuring they have access to top-quality resources for better health.",
+      title: translate(useTranslations(), "home.project.projects.3.title"),
+      description: translate(
+        useTranslations(),
+        "home.project.projects.3.description"
+      ),
     },
     {
-      title: "Logistics",
-      description:
-        "Logistics solutions provide streamlined services for the transportation of goods and resources, optimizing delivery efficiency for businesses and customers alike. The platform allows users to manage their operations seamlessly, reducing the need for time-consuming tasks like coordinating shipments or handling inventory. With secure and reliable tracking systems, it ensures safe and punctual delivery, offering smooth transportation from point A to point B.ess to top-quality resources for better health.",
+      title: translate(useTranslations(), "home.project.projects.4.title"),
+      description: translate(
+        useTranslations(),
+        "home.project.projects.4.description"
+      ),
     },
   ];
 
@@ -35,7 +45,7 @@ export default function ProjectSection() {
       )}
     >
       <h1 className="text-h2 text-woodsmoke-600 dark:text-woodsmoke-50 text-center font-bold">
-        Projects
+        {translate(useTranslations(), "home.project.title")}
       </h1>
 
       {PROJECTS.map((project, index) => (
