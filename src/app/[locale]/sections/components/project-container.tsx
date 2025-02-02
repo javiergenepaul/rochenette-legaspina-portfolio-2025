4c051f02b30159473581d45aba8332ce265ef794
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-02-02 12:29:59 +0800

    feat: add project section in home page

---
+++ src/app/[locale]/sections/components/project-container.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import React from "react";
+
+interface ProjectContainerInterface {
+  title: string;
+  description: string;
+  index: number;
+}
+
+export default function ProjectContainer({
+  title,
+  description,
+  index,
+}: ProjectContainerInterface) {
+  return (
+    <>
+      {index % 2 === 0 ? (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            type: "spring",
+            bounce: 0.5,
+          }}
+          className="flex gap-6"
+        >
+          {/* Image Component */}
+          <div
+            className="h-[500px] flex-1 w-full rounded-2xl"
+            style={{
+              background:
+                "linear-gradient(104deg, #AE3EE6 3%, #DFAEF9 49%, #9A2DCC 100%)",
+            }}
+          />
+          {/* Text Component */}
+          <div className="basis-1/2 space-y-12 bg-amethyst-100 p-8 rounded-2xl h-fit">
+            <div className="space-y-4">
+              <h2 className="text-h3 font-semibold text-woodsmoke-600">
+                {title}
+              </h2>
+              <p className="text-large font-normal text-woodsmoke-500">
+                {description}
+              </p>
+            </div>
+            <Button variant={"outline"}>Know More</Button>
+          </div>
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            type: "spring",
+            bounce: 0.5,
+          }}
+          className="flex gap-6"
+        >
+          {/* Text Component */}
+          <div className="basis-1/2 space-y-12 bg-amethyst-100 p-8 rounded-2xl h-fit mt-auto">
+            <div className="space-y-4">
+              <h2 className="text-h3 font-semibold text-woodsmoke-600">
+                {title}
+              </h2>
+              <p className="text-large font-normal text-woodsmoke-500">
+                {description}
+              </p>
+            </div>
+            <Button variant={"outline"}>Know More</Button>
+          </div>
+          {/* Gradient Component */}
+          <div className="basis-1/2 flex gap-6">
+            <div
+              className="h-[550px] flex-1 w-full rounded-2xl"
+              style={{
+                background: "linear-gradient(-143deg, #D0B0F7, #872FF3)",
+              }}
+            />
+            <div
+              className="h-[550px] flex-1 w-full rounded-2xl mt-20"
+              style={{
+                background: "linear-gradient(-143deg, #D0B0F7, #872FF3)",
+              }}
+            />
+          </div>
+        </motion.div>
+      )}
+    </>
+  );
+}
