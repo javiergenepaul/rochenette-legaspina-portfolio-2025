22c8ec56406de35755558979dae7c7dd88bf6f49
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-02-06 21:00:13 +0800

    feat: add mobile responsive for project page

--- src/app/[locale]/projects/component/project-format.tsx
+++ src/app/[locale]/projects/component/project-format.tsx
@@ -12,7 +12,7 @@ const InfoCard = ({
   return (
     <article
       className={twMerge(
-        "p-8 border border-woodsmoke-400 dark:border-woodsmoke-400 rounded-lg flex flex-col gap-6",
+        "p-8 border border-woodsmoke-400 dark:border-woodsmoke-400 rounded-lg flex flex-col SM:items-start items-center gap-6",
         className
       )}
     >
@@ -32,10 +32,10 @@ const InfoCard = ({
           strokeLinejoin="round"
         />
       </svg>
-      <h3 className="text-woodsmoke-600 font-bold text-h3 dark:text-woodsmoke-50">
+      <h3 className="text-woodsmoke-600 font-bold XL:text-h3 LG:text-h4 text-h5 dark:text-woodsmoke-50">
         {title}
       </h3>
-      <p className="text-large text-woodsmoke-500 text-wrap text-justify dark:text-woodsmoke-300">
+      <p className="XL:text-large text-normal text-woodsmoke-500 text-wrap text-justify dark:text-woodsmoke-300">
         {description}
       </p>
     </article>
@@ -52,7 +52,7 @@ export default function ProjectFormat({
   return (
     <section id={sectionId} className="h-full space-y-20">
       <div
-        className="relative bg-amethyst-950 h-[722px] w-[722px] justify-self-center rounded-lg"
+        className="relative bg-amethyst-950 XL:h-[722px] XL:w-[722px] LG:h-[647px] LG:w-[556px] MD:h-[538px] MD:w-[542px] SM:h-[514px] SM:w-[450px] XS:h-[451px] XS:w-[420px] w-full h-[484px] justify-self-center rounded-lg"
         aria-label="Placeholder image with background color"
         role="img"
       />
@@ -70,7 +70,7 @@ export default function ProjectFormat({
         </p>
       </div>
 
-      <div className="grid grid-cols-2 gap-x-6 gap-y-10">
+      <div className="grid MD:grid-cols-2 grid-cols-1 gap-x-6 gap-y-10">
         {information.map((info, index) => (
           <InfoCard
             key={index}
@@ -84,17 +84,17 @@ export default function ProjectFormat({
 
       <div className="grid grid-cols-3 gap-6">
         <div
-          className="bg-amethyst-950 h-[540px] w-full rounded-lg"
+          className="bg-amethyst-950 XL:h-[540px] LG:h-[533px] SM:h-[488px] XS:h-[541px] h-[598px] w-full rounded-lg"
           aria-label="Placeholder image with background color"
           role="img"
         />
         <div
-          className="bg-amethyst-950 h-[540px] w-full rounded-lg"
+          className="bg-amethyst-950 XL:h-[540px] LG:h-[533px] SM:h-[488px] XS:h-[541px] h-[598px] w-full rounded-lg"
           aria-label="Placeholder image with background color"
           role="img"
         />
         <div
-          className="bg-amethyst-950 h-[540px] w-full rounded-lg"
+          className="bg-amethyst-950 XL:h-[540px] LG:h-[533px] SM:h-[488px] XS:h-[541px] h-[598px] w-full rounded-lg"
           aria-label="Placeholder image with background color"
           role="img"
         />
