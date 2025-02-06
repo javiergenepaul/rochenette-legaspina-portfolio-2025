87cf6743cd1e6caefc56fd6ff8ad827ee8adb458
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-02-06 18:08:05 +0800

    feat adding resposive to project section in home page

--- src/app/[locale]/sections/components/project-container.tsx
+++ src/app/[locale]/sections/components/project-container.tsx
@@ -26,11 +26,11 @@ export default function ProjectContainer({
             type: "spring",
             bounce: 0.5,
           }}
-          className="flex gap-6"
+          className="flex gap-6 SM:flex-row flex-col"
         >
           {/* Image Component */}
           <div
-            className="h-[500px] flex-1 w-full rounded-2xl"
+            className="basis-auto SM:basis-1/2 XL:h-[500px] LG:h-[380px] MD:h-[267px] SM:h-[195px] XS:h-[321px] h-[220px] w-full rounded-2xl"
             style={{
               background:
                 "linear-gradient(104deg, #AE3EE6 3%, #DFAEF9 49%, #9A2DCC 100%)",
@@ -39,10 +39,10 @@ export default function ProjectContainer({
           {/* Text Component */}
           <div className="basis-1/2 space-y-12 bg-amethyst-100 dark:bg-woodsmoke-900 p-8 rounded-2xl h-fit">
             <div className="space-y-4">
-              <h2 className="text-h3 font-semibold text-woodsmoke-600 dark:text-woodsmoke-50">
+              <h2 className="XL:text-h3 LG:text-h4 text-h5 font-semibold text-woodsmoke-600 dark:text-woodsmoke-50">
                 {title}
               </h2>
-              <p className="text-large font-normal text-woodsmoke-500 dark:text-woodsmoke-300">
+              <p className="XL:text-large text-normal font-normal text-woodsmoke-500 dark:text-woodsmoke-300 MD:line-clamp-none SM:line-clamp-5 line-clamp-none">
                 {description}
               </p>
             </div>
@@ -58,30 +58,30 @@ export default function ProjectContainer({
             type: "spring",
             bounce: 0.5,
           }}
-          className="flex gap-6"
+          className="flex gap-6 SM:flex-row flex-col-reverse"
         >
           {/* Text Component */}
-          <div className="basis-1/2 space-y-12 bg-amethyst-100 dark:bg-woodsmoke-900 p-8 rounded-2xl h-fit mt-auto">
+          <div className="basis-1/2 space-y-12 bg-amethyst-100 dark:bg-woodsmoke-900 p-8 rounded-2xl h-fit LG:mt-auto">
             <div className="space-y-4">
-              <h2 className="text-h3 font-semibold text-woodsmoke-600 dark:text-woodsmoke-50">
+              <h2 className="XL:text-h3 LG:text-h4 text-h5 font-semibold text-woodsmoke-600 dark:text-woodsmoke-50">
                 {title}
               </h2>
-              <p className="text-large font-normal text-woodsmoke-500 dark:text-woodsmoke-300">
+              <p className="XL:text-large text-normal font-normal text-woodsmoke-500 dark:text-woodsmoke-300 LG:line-clamp-none line-clamp-5">
                 {description}
               </p>
             </div>
             <Button variant={"outline"}>Know More</Button>
           </div>
           {/* Gradient Component */}
-          <div className="basis-1/2 flex gap-6">
+          <div className="basis-1/2 flex gap-6 SM:h-auto">
             <div
-              className="h-[550px] flex-1 w-full rounded-2xl"
+              className="XL:h-[550px] LG:h-[360px] MD:h-[270px] SM:h-[230px] XS:h-[280px] h-[220px] flex-1 w-full rounded-2xl"
               style={{
                 background: "linear-gradient(-143deg, #D0B0F7, #872FF3)",
               }}
             />
             <div
-              className="h-[550px] flex-1 w-full rounded-2xl mt-20"
+              className="XL:h-[550px] LG:h-[360px] MD:h-[270px] SM:h-[230px] XS:h-[280px] h-[220px] flex-1 w-full rounded-2xl XL:mt-20 SM:mt-auto XS:mt-32 mt-20"
               style={{
                 background: "linear-gradient(-143deg, #D0B0F7, #872FF3)",
               }}
