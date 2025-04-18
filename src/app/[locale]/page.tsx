05f6c13e576f6532358e2846a38753053766c1b6
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-04-18 11:22:26 +0800

    add loading screen with zustand

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import { LetstalkSection } from "@/components";
 import { HeroSection, ProjectSection } from "./sections";
+import { useLoadingStore } from "@/store";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { setLoading } = useLoadingStore();
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
     <div className="flex flex-col gap-20 pb-20">
       <HeroSection />
